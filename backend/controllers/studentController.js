1e8068712a721d45e50a885f7c1f079618241595
e66433560-spec <e66433560@gmail.com>
2025-10-31 16:03:21 +0530

    Create studentController.js

---
+++ backend/controllers/studentController.js
@@ -0,0 +1,17 @@
+import Student from "../models/Student.js";
+
+export const getStudents = async (req, res) => {
+  const students = await Student.find();
+  res.json(students);
+};
+
+export const addStudent = async (req, res) => {
+  const student = new Student(req.body);
+  await student.save();
+  res.json(student);
+};
+
+export const deleteStudent = async (req, res) => {
+  await Student.findByIdAndDelete(req.params.id);
+  res.json({ message: "Student deleted" });
+};
